6d080edc41fac6b647dd5a5ccebe951e085508b3
Ucrash <gducrash@gmail.com>
2022-11-09 12:33:23 +0200

    make navbar fixed after some threshold

--- src/components/page/Navbar/Navbar.tsx
+++ src/components/page/Navbar/Navbar.tsx
@@ -1,8 +1,10 @@
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import classes from './Navbar.module.scss';
 import logo from '../../../assets/logo.svg';
 import { constructClass } from '../../../scripts/util';
 import Button from '../../general/Button/Button';
 import useResponsive from '../../../hooks/useResponsive';
+import useScroll from '../../../hooks/useScroll';
 
 type NavbarProps = {
     menuItems: {
@@ -13,29 +15,32 @@ type NavbarProps = {
 
 const Navbar = (props: NavbarProps) => {
 
+    const scroll = useScroll();
+
     const isMobile = useResponsive(1110);
+    const isFixed = scroll > 600;
 
     const navbarSections = {
         desktop: <>
             <div className={classes['navbar-content-section']}>
                 { props.menuItems.map((m, i) => 
-                    <a 
+                    <AnchorLink 
                         className={constructClass([
                             m.selected ? classes.selected : null
                         ])}
                         href={m.href} key={i}
-                    >{m.title}</a>    
+                    >{m.title}</AnchorLink>    
                 ) }
             </div>
 
             <div className={classes['navbar-content-section']}>
-                <Button text={props.button} white navbar />
+                <Button text={props.button} white={!isFixed} navbar />
             </div>
         </>,
 
         mobile: <>
             <div className={classes['navbar-content-section']}>
-                <Button text={props.button} white navbar />
+                <Button text={props.button} white={!isFixed} navbar />
             </div>
         </>
     }
@@ -43,7 +48,8 @@ const Navbar = (props: NavbarProps) => {
     return (
         <header className={constructClass([
             classes['navbar'],
-            isMobile ? classes['mobile'] : null
+            isMobile ? classes['mobile'] : null,
+            isFixed ? classes['fixed'] : null
         ])}>
         <div className={classes['navbar-content']}>
             
