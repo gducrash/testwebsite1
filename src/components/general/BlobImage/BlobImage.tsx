c8b77b3c32dbdac791a071fafa8ed93e6f38a059
Ucrash <gducrash@gmail.com>
2022-11-09 18:57:39 +0200

    added animated blobs

---
+++ src/components/general/BlobImage/BlobImage.tsx
@@ -0,0 +1,32 @@
+import { motion, MotionValue } from 'framer-motion';
+import { constructClass } from '../../../scripts/util';
+import classes from './BlobImage.module.scss';
+
+type BlobImageProps = {
+    variation: number,
+    width: number, height: number,
+    image: string,
+    className?: string,
+    parallax?: MotionValue
+}
+
+const BlobImage = (props: BlobImageProps) => {
+
+    return (
+        <motion.div 
+            className={constructClass([
+                classes["blob"], 
+                classes["variation-" + props.variation],
+                props.className
+            ])} 
+            style={{
+                y: props.parallax,
+                width: props.width, height: props.height,
+                backgroundImage: props.image
+            }}
+        />
+    )
+
+}
+
+export default BlobImage;
