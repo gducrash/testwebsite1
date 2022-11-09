6d1c2b662300b4997013a3cf9b0476c80b805245
Ucrash <gducrash@gmail.com>
2022-11-09 16:56:49 +0200

    add hero parrot image

--- src/components/page/Section/SectionHero.tsx
+++ src/components/page/Section/SectionHero.tsx
@@ -2,6 +2,7 @@ import classes from './Section.module.scss';
 import strings from '../../../util/strings';
 import SearchInput from '../../general/SearchInput/SearchInput';
 import ScrollTriggered from '../../general/ScrollTriggeredDiv/ScrollTriggered';
+import imgParrot from '../../../assets/parrot.svg';
 
 const SectionHero = () => {
 
@@ -11,26 +12,26 @@ const SectionHero = () => {
 
     const animatedParagraph = <ScrollTriggered type="p" delay={0.2}>
         { strings.HERO }
-    </ScrollTriggered>
+    </ScrollTriggered>;
 
     const animatedImage = <ScrollTriggered 
-        type="img" delay={0.3}
-        src="https://cdn.discordapp.com/attachments/510776441084968977/1039522281245921341/unknown.png" 
-        alt="The Wise Mystical Tree" draggable="false"
-    />
+        type="img" delay={0.3} className={classes["hero-image"]}
+        src={imgParrot}
+        alt="Parrot" draggable="false"
+    />;
 
     return (
         <section className={classes["section-hero"]} id="home">
 
-            <ScrollTriggered className={classes["section-content"]}>
+            <div className={classes["section-content"]}>
                 <div className={classes["split"]}>
-                    <div className={classes["hero-left"]}>
+                    <ScrollTriggered className={classes["hero-left"]}>
 
                         { animatedHeading } { animatedParagraph }
 
                         <SearchInput />
 
-                    </div>
+                    </ScrollTriggered>
 
                     <div className={classes["hero-right"]}>
 
@@ -38,7 +39,7 @@ const SectionHero = () => {
                         
                     </div>
                 </div>
-            </ScrollTriggered>
+            </div>
 
         </section>
     )
