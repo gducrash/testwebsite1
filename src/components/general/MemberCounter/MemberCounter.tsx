e4a8a3f29c0690e2f30c5828058495a47581202f
Ucrash <gducrash@gmail.com>
2022-11-11 22:24:06 +0200

    don't animate

--- src/components/general/MemberCounter/MemberCounter.tsx
+++ src/components/general/MemberCounter/MemberCounter.tsx
@@ -1,5 +1,3 @@
-import { useMotionValue, useSpring } from 'framer-motion';
-import { useEffect, useRef } from 'react';
 import classes from './MemberCounter.module.scss';
 import strings from '../../../util/strings';
 import { constructClass, formatCount } from '../../../scripts/util';
@@ -15,18 +13,6 @@ type MemberCounterProps = {
 
 export const MemberCounter = (props: MemberCounterProps) => {
 
-    const counterRef = useRef() as any;
-    const count = useMotionValue(props.targetCount * 0.5);
-    const animatedCount = useSpring(count, { stiffness: 50, damping: 20, bounce: 0 });
-
-    useEffect(() => {
-        const unsubscribe = animatedCount.onChange(c => {
-            counterRef.current.innerText = formatCount(c);
-        });
-        count.set(props.targetCount);
-        return unsubscribe;
-    }, []);
-
     return (
         <ScrollTriggered 
             className={constructClass([
@@ -38,7 +24,7 @@ export const MemberCounter = (props: MemberCounterProps) => {
             <img src={icHealth} alt="Member Counter Icon" draggable="false" />
             <div className={classes["members-counter-content"]}>
                 <h4>{ strings.HERO_MEMBERS }</h4>
-                <h2 ref={counterRef}>{ formatCount(props.targetCount) }</h2>
+                <h2>{ formatCount(props.targetCount) }</h2>
             </div>
         </ScrollTriggered>
     );
